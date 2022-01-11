c6168953c67e41488b8d5fb8ba7d658450832f58
Aastha Singh <btech10014.18@bitmesra.ac.in>
2022-01-12 00:09:03 +0530

    Add awake-drowsy-sleepy logic

--- detect.py
+++ detect.py
@@ -1,5 +1,7 @@
+import time
 import cv2
 from PIL import Image
+from numpy import ndindex
 import torch
 from torchvision import transforms
 from centerface import CenterFace
@@ -15,8 +17,19 @@ class Drowsiness:
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.eyes_model = torch.load('models/eyes_resnet18_128x128.pt', map_location=self.device)
         self.eyes_model.eval()
-        self.classes = {0: 'closed', 1: 'open'}
 
+        # Classes
+        self.classes = {0: 'closed', 1: 'open'}
+        self.result = {0: 'drowsy', 1: 'awake', -1: 'sleeping'}
+
+        # Score
+        self.score = 1.0
+        self.score_list = list()
+        self.score_limit = 50
+        self.count = 0
+        self.res = 1
+
+        # Resizable display window
         cv2.namedWindow("Drowsiness Detection", cv2.WINDOW_NORMAL)
 
     def classify_eye(self, crop):
@@ -37,6 +50,20 @@ class Drowsiness:
         index = self.classes[output.data.cpu().numpy().argmax()]
         
         return index
+    
+    def update_score_v2(self):
+
+        if len(self.score_list) >= self.score_limit:
+            
+            self.count = self.score_list.count('closed')
+            if self.count > 40:
+                self.res = -1
+            elif self.count < 40 and self.count > 10:
+                self.res = 0
+            else:
+                self.res = 1
+            
+            self.score_list = list()
     
     def get_detections_centerface(self, frame):
         
@@ -71,6 +98,10 @@ class Drowsiness:
 
             index_left = self.classify_eye(gray[left_eye_y1:left_eye_y2, left_eye_x1:left_eye_x2])
             index_right = self.classify_eye(gray[right_eye_y1:right_eye_y2, right_eye_x1:right_eye_x2])
+            
+            if index_left == index_right:
+                self.score_list.append(index_left)
+                self.update_score_v2()
 
             eyes.append([
                 left_eye_x1, left_eye_y1, left_eye_x2, left_eye_y2, index_left,
@@ -102,7 +133,23 @@ class Drowsiness:
                 
                 cv2.rectangle(frame, (eye[0], eye[1]), (eye[2], eye[3]), self.get_color(eye[4]), 2)
                 cv2.rectangle(frame, (eye[5], eye[6]), (eye[7], eye[8]), self.get_color(eye[9]), 2)
-                
+            
+            color = (0, 255, 0)
+
+            if self.res == 1:
+                color = (0, 255, 0)
+            elif self.res == 0:
+                color = (0, 69, 255)
+            elif self.res == -1:
+                color = (0, 0, 255)
+            else:
+                pass
+
+            # cv2.putText(frame, 'Result: {} | Score: {:.2f}'.format(res, self.score), (20, 20), 
+            #     cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 1, cv2.LINE_AA)
+
+            cv2.putText(frame, 'Result: {} | Score: {}'.format(self.result[self.res], self.count), (20, 20), 
+                cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 1, cv2.LINE_AA)
         except:
             pass
 
